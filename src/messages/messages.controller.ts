796d3a45f92efe0a1b3de1743c6fd7960608ca75
Felipe <felipe.cruz@altatec.com.mx>
2021-10-27 18:25:55 -0500

    First

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+
+    @Get()
+    listMessages(){
+
+    }
+
+    @Post()
+    createdMessaes(@Body() body:any){
+
+        console.log(body);
+    }
+
+    @Get('/:id')
+    getMessage( @Param('id') id: string){
+
+        console.log(id);
+
+    }
+
+}
